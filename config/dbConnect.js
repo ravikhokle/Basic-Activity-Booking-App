8373f5591c33a7702f2520b1a879866c09474203
ravikhokle <ravikhokle1@gmail.com>
2025-05-09 19:58:32 +0530

    fix DB

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DBConnect = async () =>{
     try {
-        await mongoose.connect(URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Conneted to database");
     } catch (error) {
         console.log("Connetion Problem:", error.message);
