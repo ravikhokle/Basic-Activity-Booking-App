6956480442b0151ef0ced224468e578d5f0b990e
ravikhokle <ravikhokle1@gmail.com>
2025-05-09 15:48:45 +0530

    Server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();  
+const port = process.env.PORT || 3000;
+
+app.get("/", (req,res)=>{
+    res.send("welcome to backend");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
